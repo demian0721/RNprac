e6031ef35f48ac4e744594c306d29c4aa8e16e93
demian0721 <83893777+demian0721@users.noreply.github.com>
2021-10-21 18:39:02 +0900

    03 컴포넌트 스타일링
    진도 현황
    ch03-1 style 속성 / StyleSheet API
    ch03-2 View 컴포넌트와 CSS 박스모델
    ch03-3 자원과 아이콘 사용하기
    코어컴포넌트 활용 : ImageBackground, Image

--- ch03_3/App.tsx
+++ ch03_3/App.tsx
@@ -1,115 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React from 'react'
+// API
+import { StyleSheet } from 'react-native'
+// Core Component
+import { ImageBackground, SafeAreaView, Image } from 'react-native'
+import * as D from './src/data'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
+const avatarUrl = D.randomAvatarUrl()
+const avatarSize = 50
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={[styles.flex]}>
+      <ImageBackground style={[styles.flex, styles.imageBackground]} source={require('./src/assets/images/bg.jpg')}>
+          <Image source={{uri: avatarUrl}} style={[styles.image]} />
+      </ImageBackground>
     </SafeAreaView>
-  );
-};
+  )
+}
+
+export default App
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+  flex: {flex: 1},
+  imageBackground: { padding: 10},
+  image: {width: avatarSize, height: avatarSize, borderRadius: avatarSize / 2}
+})
