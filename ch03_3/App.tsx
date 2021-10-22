01f17bcbf9bf3e37bb261dfc2ebb5d34ccf1548e
demian0721 <83893777+demian0721@users.noreply.github.com>
2021-10-22 12:11:36 +0900

    03 컴포넌트 스타일링
    ch03_3 자원과 아이콘 사용하기
    (폰트, 아이콘 패키지 활용)
    - react-native.conif.js
    폰트 자원을 앱에 포함하기 위한 명령인 npx react-native link 를 실행하기 위해 필요한 구성 파일

--- ch03_3/App.tsx
+++ ch03_3/App.tsx
@@ -1,17 +1,30 @@
 import React from 'react'
-// API
-import { StyleSheet } from 'react-native'
-// Core Component
-import { ImageBackground, SafeAreaView, Image } from 'react-native'
+// Packages
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import {Colors} from 'react-native-paper'
 import * as D from './src/data'
+// API
+import { StyleSheet, Platform, Alert } from 'react-native'
+// Core Component
+import { SafeAreaView, View, ImageBackground, Image, Text } from 'react-native'
+
 
 const avatarUrl = D.randomAvatarUrl()
 const avatarSize = 50
+const text = 'Alost before we kew it, we had left the ground'
+
 const App = () => {
   return (
     <SafeAreaView style={[styles.flex]}>
       <ImageBackground style={[styles.flex, styles.imageBackground]} source={require('./src/assets/images/bg.jpg')}>
           <Image source={{uri: avatarUrl}} style={[styles.image]} />
+          <View style={[styles.flex, styles.padding10]}>
+            <Text style={[styles.text, styles.regular]}>{text} [regular]</Text>
+            <Text style={[styles.text, styles.medium]}>{text} [medium]</Text>
+            <Text style={[styles.text, styles.semiBold]}>{text} [semiBold]</Text>
+            <Text style={[styles.text, styles.bold]}>{text} [bold]</Text>
+          </View>
+          <Icon name="home" size={50} color={Colors.lightBlue500} onPress={() => Alert.alert('icon pressed')} />
       </ImageBackground>
     </SafeAreaView>
   )
@@ -22,5 +35,15 @@ export default App
 const styles = StyleSheet.create({
   flex: {flex: 1},
   imageBackground: { padding: 10},
-  image: {width: avatarSize, height: avatarSize, borderRadius: avatarSize / 2}
+  image: {width: avatarSize, height: avatarSize, borderRadius: avatarSize / 2},
+  padding10: {padding: 10},
+  text: {textAlign: 'center', fontSize: 25, color: 'white', marginBottom: 10},
+
+  regular: {fontFamily: 'DancingScript-Regular', fontWeight: '400'},
+  medium: {fontFamily: 'DancingScript-Medium', fontWeight: '500'},
+  semiBold: {fontFamily: 'DancingScript-SemiBold', fontWeight: '600'},
+  bold: {
+    fontFamily: 'DancingScript-Bold', 
+    fontWeight: Platform.select({ios:'700', android:'600'})
+  },
 })
